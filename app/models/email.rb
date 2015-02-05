a5d35615e85ffd0714ee8e6b025abb33a8b9cedb
tiago <tiago.ameller@gmail.com>
2015-02-05 20:47:00 +0100

    stable. previous a alphabet index

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,9 @@
 class Email < ActiveRecord::Base
   belongs_to :person
-  validates :address, presence: true
+  validates :address, 
+    presence: true,
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  def display_text
+    self.address
+  end
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,10 @@
 class Phone < ActiveRecord::Base
   belongs_to :person
-  validates :address, presence: true
+  validates :address, 
+    presence: true,
+    format: { with: /\A[0-9\.\(\)\+-]+\z/i }
+
+  def display_text
+    self.address
+  end
 end
